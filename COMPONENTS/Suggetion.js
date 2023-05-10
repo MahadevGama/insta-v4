baa0393987b9609d0e545f8bb4b7ec8826846126
MahadevGama <mahadevgama21@gmail.com>
2023-05-10 10:28:46 +0530

    install next-auth and firebase and initlize the firebase

--- COMPONENTS/Suggetion.js
+++ COMPONENTS/Suggetion.js
@@ -5,7 +5,7 @@ export default function Suggetion() {
     const [suggetion, setSuggetion] = useState([]);
     
     useEffect(()=>{
-        const suggetion=minifaker.array(5,(i)=>({
+        const suggetion=minifaker.array(7,(i)=>({
 
             username:minifaker.username({locale:"en"}).toLowerCase(),
             jobTitle: minifaker.jobTitle(),
@@ -21,11 +21,11 @@ export default function Suggetion() {
         <button className='text-gray-400 font-semibold mr-2'>See All</button>
       </div>
       {suggetion.map(suggetion=>(
-        <div className="flex items-center justify-between mt-3 bg-white">
+        <div className="flex items-center justify-between mt-3 bg-white ">
           <img className='h-10 rounded-full border p-[2px]' src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random()*70)}`} alt="" />
           <div className="flex-1 ml-6">
             <h2 className=' font-semibold text-sm'>{suggetion.username}</h2>
-            <h3 className='text-sm text-gray-200 truncate w-[230px]'>{suggetion.jobTitle}</h3>
+            <h3 className='text-sm text-blue-600 truncate w-[230px]'>{suggetion.jobTitle}</h3>
           </div>
           <button className='text-blue-800 font-semibold text-sm'>Fallow</button>
         </div>
